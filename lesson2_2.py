da807d76c87721bbe020787914cfd42995305c10
MercOUSE <mercOUSE@yandex.ru>
2022-07-03 19:40:36 +0300

    Operations

--- lesson2_2.py
+++ lesson2_2.py
@@ -1,1 +1,35 @@
 # Пример 6
+x = 2
+y = 8
+
+print(x + y)
+print(x + 3)
+print(x - y)
+
+print()
+
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+
+print()
+
+print(x ** y)
+print(x ** 0.5)
+print(pow(x,y))
+
+print ()
+
+z = -2
+print(abs(z))
+print(z ** 2)
+
+print()
+
+print(3.2 * 0.8 - 2 * 5 - 3 ** 3)
+
+print()
+
+number = 3.26
+print(round(number), round(number,1))
