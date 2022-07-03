912f323d33ff4ebe8c566e89a34a86e8585f76e3
MercOUSE <mercOUSE@yandex.ru>
2022-07-03 20:05:42 +0300

    String2

---
+++ lesson2_5.py
@@ -0,0 +1,10 @@
+# Пример 9
+string = input('Enter a string: ')
+print('You have entered "{}"'.format(string))
+
+print('Press Enter to continue...')
+input()
+
+n = int(input('First number: '))
+m = int(input('Second number: '))
+print('{} + {} = {}'.format(n, m, n + m))
