5ca3a14db5a4ad855fef0e62761adc04c9fb6e8b
MercOUSE <mercOUSE@yandex.ru>
2022-07-05 15:44:58 +0300

    poly

---
+++ lesson4_3.py
@@ -0,0 +1,13 @@
+# Пример 18
+x = int(input("x = "))
+
+if 0 < x < 7:
+    print("Value is in range, let's continue")
+    y = 2 * x - 5
+    if y < 0:
+        print("y is negative")
+    else:
+        if y > 0:
+            print("y is positive")
+        else:
+            print("y = 0")
